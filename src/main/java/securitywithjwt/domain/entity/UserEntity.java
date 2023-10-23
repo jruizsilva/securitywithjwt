e8b2553c7bb077c369ec69fa923ba1623040d5a2
jruizsilva <ruizsilvajonathan@gmail.com>
2023-10-23 13:21:29 -0300

    SecurityBeansInjector ADDED

--- src/main/java/securitywithjwt/domain/entity/UserEntity.java
+++ src/main/java/securitywithjwt/domain/entity/UserEntity.java
@@ -2,7 +2,14 @@ package securitywithjwt.domain.entity;
 
 import jakarta.persistence.*;
 import lombok.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 import securitywithjwt.common.util.Role;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Builder
 @AllArgsConstructor
@@ -12,7 +19,7 @@ import securitywithjwt.common.util.Role;
 @ToString
 @Entity
 @Table(name = "users")
-public class UserEntity {
+public class UserEntity implements UserDetails {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id",
@@ -25,4 +32,33 @@ public class UserEntity {
     @Enumerated(EnumType.STRING)
     private Role role;
 
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        List<GrantedAuthority> authorities = role.getPermissions()
+                                                 .stream()
+                                                 .map(permissionEnum -> new SimpleGrantedAuthority(permissionEnum.name()))
+                                                 .collect(Collectors.toList());
+        authorities.add(new SimpleGrantedAuthority("ROLE_" + role.name()));
+        return authorities;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
--- src/main/java/securitywithjwt/persistence/UserRepository.java
+++ src/main/java/securitywithjwt/persistence/UserRepository.java
@@ -1,7 +1,14 @@
 package securitywithjwt.persistence;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.security.core.userdetails.UserDetails;
 import securitywithjwt.domain.entity.UserEntity;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<UserEntity, Long> {
+    @Query("select u from UserEntity u where upper(u.username) = upper(:username)")
+    Optional<UserEntity> findByUsernameIgnoreCase(@Param("username") String username);
 }
